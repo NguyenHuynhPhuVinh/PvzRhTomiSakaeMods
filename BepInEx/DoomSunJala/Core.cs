6b6a778dd3f9acb8143d9fb5291551c6b9e074ac
TomiSakae <tomisakaeap3@gmail.com>
2025-10-09 20:22:01 +0700

    Update Core.cs for improved logic and version increment to 1.7.0

--- BepInEx/DoomSunJala/Core.cs
+++ BepInEx/DoomSunJala/Core.cs
@@ -1,4 +1,4 @@
-﻿// --- START OF FILE Core.cs --- Của mod DoomSunJala (Sửa lỗi theo mẫu NullNut)
+﻿// --- START OF FILE Core.cs --- Của mod DoomSunJala (Logic "Hồi Sinh")
 
 using BepInEx;
 using BepInEx.Unity.IL2CPP;
@@ -12,15 +12,9 @@ using UnityEngine;
 
 namespace DoomSunJala
 {
-    // THAY ĐỔI: ĐÃ XÓA ATTRIBUTE [RegisterTypeInIl2Cpp]
     public class DoomSunJalaComponent : MonoBehaviour
     {
-        // THAY ĐỔI: Thêm 2 constructor giống hệt mẫu NullNut
-        public DoomSunJalaComponent() : base(ClassInjector.DerivedConstructorPointer<DoomSunJalaComponent>())
-        {
-            ClassInjector.DerivedConstructorBody(this);
-        }
-        public DoomSunJalaComponent(IntPtr i) : base(i) { }
+        public DoomSunJalaComponent(IntPtr ptr) : base(ptr) { }
     }
 
     [BepInPlugin(Core.PluginGUID, Core.PluginName, Core.PluginVersion)]
@@ -28,7 +22,7 @@ namespace DoomSunJala
     {
         public const string PluginGUID = "com.tomisakae.doomsunjala";
         public const string PluginName = "PvzRhTomiSakaeMods - DoomSunJala";
-        public const string PluginVersion = "1.0.0";
+        public const string PluginVersion = "1.7.0"; // Tăng phiên bản
 
         public static Core Instance;
 
@@ -37,10 +31,7 @@ namespace DoomSunJala
         public override void Load()
         {
             Instance = this;
-
-            // THAY ĐỔI: Đăng ký component thủ công giống hệt mẫu NullNut
             ClassInjector.RegisterTypeInIl2Cpp<DoomSunJalaComponent>();
-
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
 
             var ab = CustomCore.GetAssetBundle(Assembly.GetExecutingAssembly(), "doomsunjala");
@@ -60,7 +51,8 @@ namespace DoomSunJala
 
             string plantName = "Hướng Dương Hủy Diệt Tối Thượng";
             string plantDescription =
-                "Khi tạo ra ánh nắng, nó sẽ triệu hồi các cây Ớt Hủy Diệt ra các ô trống xung quanh.\n" +
+                "Khi tạo ra ánh nắng, nó sẽ triệu hồi một cây Ớt Hủy Diệt ngẫu nhiên ra ô trống xung quanh.\n" +
+                "Miễn nhiễm với sát thương nổ từ Ớt Hủy Diệt.\n" +
                 "Sản lượng: <color=red>100 ánh nắng/25 giây</color>\n" +
                 "Triệu hồi: <color=red>Ớt Hủy Diệt</color>\n" +
                 "Công thức: <color=red>Ớt Hủy Diệt + Hướng Dương Hủy Diệt</color>\n\n" +
@@ -73,45 +65,95 @@ namespace DoomSunJala
 
         #region Harmony Patches
 
-        [HarmonyPatch(typeof(Producer), "ProduceSun")]
-        public static class Producer_ProduceSun_Patch
+        // Logic triệu hồi giữ nguyên
+        [HarmonyPatch(typeof(DoomSunflower), nameof(DoomSunflower.ProduceSun))]
+        public static class DoomSunflower_ProduceSun_Patch
         {
-            public static void Postfix(Producer __instance)
+            public static bool Prefix(DoomSunflower __instance)
             {
-                if (__instance == null || __instance.thePlantType != (PlantType)Core.DoomSunJalaPlantId) return;
-                if (Board.Instance == null || CreatePlant.Instance == null)
-                {
-                    Instance.Log.LogError("Board hoặc CreatePlant instance là null!");
-                    return;
-                }
-
-                int plantColumn = __instance.thePlantColumn;
-                int plantRow = __instance.thePlantRow;
-
-                Instance.Log.LogInfo($"Cây {__instance.thePlantType} tại ({plantColumn},{plantRow}) đang cố triệu hồi...");
-
+                if (__instance == null || __instance.GetComponent<DoomSunJalaComponent>() == null) return true;
+                if (Board.Instance == null || CreatePlant.Instance == null) return false;
+                CreateItem.Instance.SetCoin(__instance.thePlantColumn, __instance.thePlantRow, (int)SunType.BigSun, 0, __instance.transform.position, false);
+                List<Vector2Int> emptySpots = new List<Vector2Int>();
                 for (int c_offset = -1; c_offset <= 1; c_offset++)
                 {
                     for (int r_offset = -1; r_offset <= 1; r_offset++)
                     {
                         if (c_offset == 0 && r_offset == 0) continue;
-
-                        int targetColumn = plantColumn + c_offset;
-                        int targetRow = plantRow + r_offset;
-
+                        int targetColumn = __instance.thePlantColumn + c_offset;
+                        int targetRow = __instance.thePlantRow + r_offset;
                         if (CreatePlant.Instance.CheckBox(targetColumn, targetRow, PlantType.DoomJalapeno))
                         {
-                            try
-                            {
-                                CreatePlant.Instance.SetPlant(targetColumn, targetRow, PlantType.DoomJalapeno, null, default, true, true, null);
-                                Instance.Log.LogInfo($"Đã triệu hồi DoomJalapeno tại ({targetColumn},{targetRow})");
-                            }
-                            catch (Exception ex)
-                            {
-                                Instance.Log.LogError($"Lỗi khi triệu hồi DoomJalapeno tại ({targetColumn},{targetRow}): {ex.Message}");
-                            }
+                            emptySpots.Add(new Vector2Int(targetColumn, targetRow));
                         }
                     }
+                }
+                if (emptySpots.Count > 0)
+                {
+                    Vector2Int randomSpot = emptySpots[UnityEngine.Random.Range(0, emptySpots.Count)];
+                    CreatePlant.Instance.SetPlant(randomSpot.x, randomSpot.y, PlantType.DoomJalapeno, null, default, true, true, null);
+                }
+                return false;
+            }
+        }
+
+        // --- LOGIC HỒI SINH MỚI ---
+        [HarmonyPatch(typeof(DoomJalapeno), nameof(DoomJalapeno.AnimExplode))]
+        public static class DoomJalapeno_AnimExplode_Patch
+        {
+            // Danh sách để lưu tọa độ các cây DoomSunJala trước khi nổ
+            private static List<Vector2Int> doomSunJalaPositions = new List<Vector2Int>();
+
+            [HarmonyPrefix]
+            public static void Prefix()
+            {
+                doomSunJalaPositions.Clear();
+                try
+                {
+                    if (Board.Instance == null || Board.Instance.plantArray == null) return;
+
+                    foreach (Plant plant in Board.Instance.plantArray)
+                    {
+                        if (plant != null && plant.GetComponent<DoomSunJalaComponent>() != null)
+                        {
+                            doomSunJalaPositions.Add(new Vector2Int(plant.thePlantColumn, plant.thePlantRow));
+                            Instance.Log.LogInfo($"[HỒI SINH] Đã lưu vị trí của DoomSunJala tại ({plant.thePlantColumn}, {plant.thePlantRow})");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Instance.Log.LogError($"Lỗi trong Prefix hồi sinh: {ex}");
+                }
+            }
+
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                try
+                {
+                    if (Board.Instance == null || CreatePlant.Instance == null || doomSunJalaPositions.Count == 0) return;
+
+                    foreach (Vector2Int position in doomSunJalaPositions)
+                    {
+                        // Kiểm tra xem ô đó có còn cây nào không
+                        Plant plantAtPosition = Lawnf.GetPlant(position.x, position.y, Board.Instance);
+                        if (plantAtPosition == null)
+                        {
+                            // Nếu ô trống, nghĩa là cây đã bị phá hủy -> trồng lại
+                            Instance.Log.LogInfo($"[HỒI SINH] Phát hiện DoomSunJala tại ({position.x}, {position.y}) đã bị phá hủy. Trồng lại...");
+                            CreatePlant.Instance.SetPlant(position.x, position.y, (PlantType)Core.DoomSunJalaPlantId, null, default, true, false, null);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Instance.Log.LogError($"Lỗi trong Postfix hồi sinh: {ex}");
+                }
+                finally
+                {
+                    // Dọn dẹp danh sách sau khi dùng xong
+                    doomSunJalaPositions.Clear();
                 }
             }
         }
@@ -119,4 +161,3 @@ namespace DoomSunJala
         #endregion
     }
 }
-// --- END OF FILE Core.cs ---
